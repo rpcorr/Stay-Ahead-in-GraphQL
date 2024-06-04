a4d5636fde6ae8b32bb0ef9370184d59a09b8339
Ronan Corr <ronan_corr@hotmail.com>
2024-06-04 10:47:08 -0400

    Adding a enumeration type

--- 01-Learning-GraphQL/04-Creating-a-GraphQL-Server-Schema/ski-day-counter/index.js
+++ 01-Learning-GraphQL/04-Creating-a-GraphQL-Server-Schema/ski-day-counter/index.js
@@ -1,21 +1,29 @@
-const  { ApolloServer, gql } = require("apollo-server");
+const { ApolloServer, gql } = require('apollo-server');
 
 const typeDefs = gql`
-    type SkiDay {
-        id: ID!
-        date: String!
-        mountain: String!
-    }
+  type SkiDay {
+    id: ID!
+    date: String!
+    mountain: String!
+    conditions: Conditions
+  }
 
-    type Query {
-        totalDays: Int!
-        allDays: [SkiDay!]!
-    }
+  enum Conditions {
+    POWDER
+    HEAVY
+    ICE
+    THIN
+  }
+
+  type Query {
+    totalDays: Int!
+    allDays: [SkiDay!]!
+  }
 `;
 
 const server = new ApolloServer({
-    typeDefs,
-    mocks: true
+  typeDefs,
+  mocks: true,
 });
 
-server.listen().then(({url}) => console.log(`Server running at ${url}`));
+server.listen().then(({ url }) => console.log(`Server running at ${url}`));
