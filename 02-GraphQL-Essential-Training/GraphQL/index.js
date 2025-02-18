451b8f97c6da15cced4305498f2633f1b845aa8f
Ronan Corr <ronan_corr@hotmail.com>
2025-02-17 22:30:20 -0500

    the initial GraphQL setup

--- 02-GraphQL-Essential-Training/GraphQL/index.js
+++ 02-GraphQL-Essential-Training/GraphQL/index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import schema from './data/schema';
 
 const PORT = 8080;
 
@@ -8,6 +10,17 @@ app.get('/', (req, res) => {
   res.send('GraphQL is amazing!');
 });
 
+const root = { hello: () => "Hi, I'm Ronan!" };
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
 app.listen(PORT, () =>
-  console.log(`Running server on localhost ${PORT}/graphql`)
+  console.log(`Running server on localhost:${PORT}/graphql`)
 );
