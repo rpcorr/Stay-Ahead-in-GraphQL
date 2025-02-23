3691b1b043ec25c24121767240348c334fde5eb2
Ronan Corr <ronan_corr@hotmail.com>
2025-02-23 09:58:52 -0500

    update resolvers for MongoDB

--- 02-GraphQL-Essential-Training/GraphQL/data/resolvers.js
+++ 02-GraphQL-Essential-Training/GraphQL/data/resolvers.js
@@ -1,26 +1,36 @@
-class Product {
-  constructor(id, { name, description, price, soldout, inventory, stores }) {
-    this.id = id;
-    this.name = name;
-    this.description = description;
-    this.price = price;
-    this.soldout = soldout;
-    this.inventory = inventory;
-    this.stores = stores;
-  }
-}
+import { Widgets } from './dbConnectors';
 
-const productDatabase = {};
+// class Product {
+//   constructor(id, { name, description, price, soldout, inventory, stores }) {
+//     this.id = id;
+//     this.name = name;
+//     this.description = description;
+//     this.price = price;
+//     this.soldout = soldout;
+//     this.inventory = inventory;
+//     this.stores = stores;
+//   }
+// }
+
+// const productDatabase = {};
 
 const resolvers = {
-  getProduct: ({ id }) => {
-    return new Product(id, productDatabase[id]);
+  // getProduct: ({ id }) => {
+  //   return new Product(id, productDatabase[id]);
+  // },
+  getProduct: async ({ id }) => {
+    try {
+      const product = await Widgets.findById(id);
+      return product;
+    } catch (error) {
+      throw new Error(error);
+    }
   },
 
   createProduct: ({ input }) => {
-    let id = require('crypto').randomBytes(10).toString('hex');
-    productDatabase[id] = input;
-    return new Product(id, input);
+    // let id = require('crypto').randomBytes(10).toString('hex');
+    // productDatabase[id] = input;
+    // return new Product(id, input);
   },
 };
 
