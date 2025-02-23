031c4ab30e75b838f0c96b441535f2479f6b1232
Ronan Corr <ronan_corr@hotmail.com>
2025-02-23 11:07:48 -0500

    data persistence with SQL

--- 02-GraphQL-Essential-Training/GraphQL/data/dbConnectors.js
+++ 02-GraphQL-Essential-Training/GraphQL/data/dbConnectors.js
@@ -1,4 +1,7 @@
 import mongoose from 'mongoose';
+import { Sequelize, DataTypes } from 'sequelize';
+import _ from 'lodash';
+import casual from 'casual';
 
 async function connectMongo() {
   try {
@@ -22,4 +25,34 @@ const widgetSchema = new mongoose.Schema({
 
 const Widgets = mongoose.model('widgets', widgetSchema);
 
-export { Widgets };
+const sequelize = new Sequelize('sqlite::memory:');
+
+const Categories = sequelize.define('categories', {
+  category: DataTypes.STRING,
+  description: DataTypes.STRING,
+});
+
+async function syncAndSeedCategories() {
+  try {
+    await sequelize.sync({ force: true });
+    console.log('SQLite connection established and Categories model synced.');
+
+    // Seed categories
+    await Promise.all(
+      _.times(5, () => {
+        return Categories.create({
+          category: casual.word,
+          description: casual.sentence,
+        });
+      })
+    );
+
+    console.log('Categories seeded');
+  } catch (error) {
+    console.log('Error with SQLite DB: ', error);
+  }
+}
+
+syncAndSeedCategories();
+
+export { Widgets, Categories };
